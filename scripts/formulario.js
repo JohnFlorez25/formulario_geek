a41e7bda50b858c292181058ccbb2ea4d63656ae
John Faber Flórez <john.florez25@gmail.com>
2021-11-17 15:50:12 -0500

    Mostrando por consola el nombre capturado en el input

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -1,1 +1,27 @@
-console.log("Mi ejercicio de formulario con storage")
+console.log("Mi ejercicio de formulario con storage");
+
+//Definir variables
+let miNombre = "";
+
+//Prevenir que se realice la acción SUBMIT
+document.getElementById('form')
+    .addEventListener(
+            'submit', 
+            function formSumbit(event){
+              event.preventDefault();
+            }
+    );
+
+//Agregar un evento al boton enviar "btnEnviar"
+let buttonEnviar = document.getElementById('btnEnviar');
+buttonEnviar.addEventListener('click', capturarDatos);
+function capturarDatos(){
+    console.log("El evento CLICK se está disparando");
+    //Captura la información del input relacionado con el Nombre;
+    miNombre = document.getElementById("inputNombre").value;
+
+    //imprimir lo que se captura del input
+    console.log(miNombre);
+}
+
+
